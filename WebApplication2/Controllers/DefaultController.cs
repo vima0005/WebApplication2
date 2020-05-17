e1e2531951adf5911070c7a2613cb5e216433371
Victor Magnusson <vima0005@student.hv.se>
2020-05-17 02:16:42 +0200

    testade

---
+++ WebApplication2/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            int i = 0;
+            return View();
+        }
+    }
+}
